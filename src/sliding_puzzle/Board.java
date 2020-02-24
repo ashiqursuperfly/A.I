020f3fdc94d209bae15a1d15dbde423f001d12c7
ashiqur.superfly <ashiqur.buet16@gmail.com>
2020-02-25 03:43:51 +0600

    save blankPos

--- src/sliding_puzzle/Board.java
+++ src/sliding_puzzle/Board.java
@@ -9,9 +9,21 @@ import java.util.Arrays;
  */
 
 public class Board {
-    private BoardItem[][] board;
 
-    private boolean validateInput(int n, BoardItem[][] board){
+    enum BoardConsts {
+        BLANK("B"), UP("U"), DOWN("D"), L("L"), R("R");
+
+        public String value;
+
+        BoardConsts(String value) {
+            this.value = value;
+        }
+        }
+
+    private BoardItem[][] board;
+    private int blankPositionRow, blankPositionCol;
+
+    private boolean validateInput(int n, BoardItem[][] board) {
         var size = Math.sqrt(n + 1);
         int x = board[0].length;
 
@@ -27,23 +39,30 @@ public class Board {
     }
 
     public Board(int n, BoardItem[][] board) {
-        if(validateInput(n, board)) this.board = board;
+        if (validateInput(n, board)) this.board = board;
     }
 
-    public Board(String[] puzzledListOfValues){
+    public Board(String[] puzzledListOfValues) {
         int len = puzzledListOfValues.length;
-        int size = (int)Math.sqrt(len + 1);
+        int size = (int) Math.sqrt(len + 1);
         this.board = new BoardItem[size][size];
 
-        if(!validateInput(len-1,board))return;
+        if (!validateInput(len - 1, board)) return;
 
         int row = 0;
         for (int k = 0; k < len; k++) {
-            board[row][k - row* size] = new BoardItem(puzzledListOfValues[k]);
-            if( (k+1) % size == 0)row++;
+            board[row][k - row * size] = new BoardItem(puzzledListOfValues[k]);
+            if (puzzledListOfValues[k].equals(BoardConsts.BLANK.value)) {
+                blankPositionRow = row;
+                blankPositionCol = k - row * size;
+            }
+
+            if ((k + 1) % size == 0) row++;
+
+
         }
 
-        System.out.println("Problem:\n" + this);
+        System.out.println("Problem:\n" + this + "\n" + "Blank("+blankPositionRow+","+blankPositionCol+")\n");
     }
 
     public void solve() {
@@ -57,7 +76,7 @@ public class Board {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
                 sb.append(board[i][j]);
-                if(j+1 != board.length)sb.append(".....");
+                if (j + 1 != board.length) sb.append(".....");
             }
             sb.append('\n');
         }
