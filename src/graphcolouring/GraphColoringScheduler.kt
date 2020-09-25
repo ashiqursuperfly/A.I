a63aa2502619920f36ad71dc59e742801d497618
ashiqur.superfly <ashiqur.buet16@gmail.com>
2020-09-26 02:33:04 +0600

    fix maxheap bug

--- src/graphcolouring/GraphColoringScheduler.kt
+++ src/graphcolouring/GraphColoringScheduler.kt
@@ -8,12 +8,11 @@ import kotlin.collections.ArrayList
 class GraphColoringScheduler(
     var graph: CourseGraph
 ) {
-
-    private lateinit var priorityQueue: PriorityQueue<Course>
+    var colorsUsed = 0
 
     fun execute() {
-        var colorsUsed = 0
-        priorityQueue = PriorityQueue(BrelazComparator())
+        colorsUsed = 0
+        val priorityQueue = PriorityQueue(BrelazComparator().reversed())
 
         for (item in graph.courses) {
             priorityQueue.add(item.value)
@@ -22,7 +21,7 @@ class GraphColoringScheduler(
         while (priorityQueue.isNotEmpty()) {
             val nextToColor = priorityQueue.poll()
 
-            val availableColors = getAvailableColors(colorsUsed)
+            val availableColors = getAvailableColors()
 
             for (item in nextToColor.neighbours) {
                 if (item.color != UNCOLORED) availableColors.remove(item.color)
@@ -34,18 +33,19 @@ class GraphColoringScheduler(
             else {
                 availableColors[0]
             }
-            println(colorsUsed)
+
             nextToColor.color = selectedColor
 
             priorityQueue.add(nextToColor)
             priorityQueue.remove(nextToColor)
 
         }
-
     }
 
-    private fun getAvailableColors (colorsUsed: Int) : ArrayList<Int> {
-        return ArrayList(IntArray(colorsUsed) { i -> i }.asList())
+    private fun getAvailableColors () : ArrayList<Int> {
+        val colors = ArrayList(IntArray(colorsUsed) { i -> i }.asList())
+        colors.shuffle()
+        return colors
     }
 
 }
--- src/graphcolouring/Main.kt
+++ src/graphcolouring/Main.kt
@@ -4,24 +4,31 @@ import kotlin.test.assertEquals
 
 fun main () {
 
-    val courseFile = "car-f-92.crs"
-    val studentFile = "car-f-92.stu"
 
+    val courseFiles = arrayOf("car-s-91.crs", "car-f-92.crs", "kfu-s-93.crs", "tre-s-92.crs", "yor-f-83.crs")
+    val studentFiles = arrayOf("car-s-91.stu", "car-f-92.stu", "kfu-s-93.stu", "tre-s-92.stu", "yor-f-83.stu")
 
-    val courseGraph = CourseGraph(
-        courseFile = "/home/user/Workspaces/Java/intellij/A_i/src/graphcolouring/solving_timetabling/ai_lab_class/${courseFile}",
-        studentFile = "/home/user/Workspaces/Java/intellij/A_i/src/graphcolouring/solving_timetabling/ai_lab_class/${studentFile}"
-    )
+    for (j in 0..10) {
+        for (i in courseFiles.indices) {
 
-    courseGraph.processCourseFile()
-    courseGraph.processStudentFile()
+            val courseGraph = CourseGraph(
+                courseFile = "/home/user/Workspaces/Java/intellij/A_i/src/graphcolouring/solving_timetabling/ai_lab_class/${courseFiles[i]}",
+                studentFile = "/home/user/Workspaces/Java/intellij/A_i/src/graphcolouring/solving_timetabling/ai_lab_class/${studentFiles[i]}"
+            )
 
-    for (item in courseGraph.courses) {
-        assertEquals(item.value.studentCount, item.value.studentIds.size)
+            courseGraph.processCourseFile()
+            courseGraph.processStudentFile()
+
+            for (item in courseGraph.courses) {
+                assertEquals(item.value.studentCount, item.value.studentIds.size)
+            }
+
+            val solver = GraphColoringScheduler(courseGraph)
+            solver.execute()
+            println("${courseFiles[i]},${solver.colorsUsed}")
+        }
+        println()
+        println()
     }
-
-    val solver = GraphColoringScheduler(courseGraph)
-    solver.execute()
-
 
 }
