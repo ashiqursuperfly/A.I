da38b3db752fbb532c12e90cb0bcee739fd4f8ab
ashiqur.superfly <ashiqur.buet16@gmail.com>
2020-11-28 18:04:36 +0600

    refactor

--- src/latin_square_completion/Main.kt
+++ src/latin_square_completion/Main.kt
@@ -33,7 +33,7 @@ fun main() {
     val latinSquare = LatinSquare(data = latinSquareInitData)
     println(latinSquare)
 
-    LatinSquareSolver.heuristic = Constants.LatinSquareHeuristic.MAX_DYNAMIC_DEGREE
+    LatinSquareSolver.heuristic = Constants.LatinSquareHeuristic.BRELAZ
 
     val startTime = System.currentTimeMillis()
     LatinSquareSolver.solve(latinSquare, true)
--- src/latin_square_completion/heuristics/BrelazSDFMDD.kt
+++ src/latin_square_completion/heuristics/BrelazSDFMDD.kt
@@ -4,7 +4,6 @@ import latin_square_completion.LatinRowCompareData
 
 class BrelazSDFMDD : Comparator<LatinRowCompareData> {
 
-
     override fun compare(o1: LatinRowCompareData, o2: LatinRowCompareData): Int {
         val sdf = SmallestDomainFirst()
         val c1 = sdf.calculateValue(o1)
@@ -15,10 +14,8 @@ class BrelazSDFMDD : Comparator<LatinRowCompareData> {
         }
 
         val mdd = MaxDynamicDegree()
-        val c11 = mdd.calculateValue(o1)
-        val c12 = mdd.calculateValue(o2)
 
-        return c11.compareTo(c12)
+        return mdd.compare(o1, o2)
     }
 
 
